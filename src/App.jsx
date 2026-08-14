db5d95742048ffaba1feb1c81d609ff86441f6ea
Longjie Deng <longjied@uci.edu>
2026-08-14 13:43:25 -0700

    Init: Framework of react

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'; 
+function GroceryItem (props) {
+	const [quantity, setQuantity] = useState(1); 
+	
+	return (
+		<li className = "fruit-item">
+			{props.name} - Quantity : {quantity}
+			<button onClick = { () => setQuantity(quantity+1)}>Add</button>
+		</li>
+		
+	);
+}
+
+function App() { 
+	const [groceries, setGroceries] = useState(["apple", "banana", "orange"]);
+	const [inputValue, setInputValue] = useState("");
+
+	function handleAdd() {
+		setGroceries([...groceries, inputValue]);
+		setInputValue("");
+	}
+	return ( 
+		<div> 
+			<input
+				type = "text"
+				value = {inputValue}
+				onChange = {(e) => setInputValue(e.target.value)}
+			/>
+			<button onClick={handleAdd}>
+				Add Item
+			</button>
+			<ul>
+				{groceries.map(item => <GroceryItem key = {item} name = {item} />)}
+			</ul>
+		</div> 
+	); 
+}
+
+export default App;
