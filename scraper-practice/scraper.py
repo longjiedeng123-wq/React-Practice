681014ab5a09caf539c04b62bf3251e2f4a5584e
Longjie Deng <longjied@uci.edu>
2026-08-17 13:31:46 -0700

    refactor: enclose scraper module into a single function

--- scraper-practice/scraper.py
+++ scraper-practice/scraper.py
@@ -1,29 +1,41 @@
 from playwright.sync_api import sync_playwright
 import requests
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://h5.awsprod.99ranch.com/stores/ad/1009")
-    
-    image_elements = page.locator("img[alt='poster']").all()
+def scrape_ad_images():
+    image_paths = []
 
-    count = 1
-
-    for image_element in image_elements:
-
-        partial_url = image_element.get_attribute("src")
-    
-        full_url = "https://h5.awsprod.99ranch.com" + partial_url
-        print("Downloading from:", full_url)
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://h5.awsprod.99ranch.com/stores/ad/1009")
         
-        response = requests.get(full_url)
-        
-        with open(f"weekly_ad_{count}.jpg", "wb") as file:
-            file.write(response.content)
+        image_elements = page.locator("img[alt='poster']").all()
+
+        count = 1
+
+        for image_element in image_elements:
+
+            partial_url = image_element.get_attribute("src")
+
+            if not partial_url:
+                print("This Poster not found")
+                continue
+
+            full_url = "https://h5.awsprod.99ranch.com" + partial_url
+            print("Downloading from:", full_url)
             
-        print("Image downloaded successfully!")
+            response = requests.get(full_url)
+            image_name = f"weekly_ad_{count}.jpg"
 
-        count += 1
-    
-    browser.close()
+            with open(image_name, "wb") as file:
+                file.write(response.content)
+
+            image_paths.append(image_name)
+
+            print("Image downloaded successfully!")
+
+            count += 1
+        
+        browser.close()
+
+        return image_paths
